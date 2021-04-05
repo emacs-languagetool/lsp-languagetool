3ac78faef65dd0f6af6f804f08beba54c3333625
JenChieh <jcs090218@gmail.com>
2021-04-05 23:35:36 +0800

    Add core

---
+++ lsp-languagetool.el
@@ -0,0 +1,46 @@
+;;; lsp-languagetool.el --- LSP Client for LanguageTool  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-04-05 23:30:46
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: LSP Client for LanguageTool.
+;; Keyword: lsp languagetool checker
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/emacs-languagetool/lsp-languagetool
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; LSP server implementation for LanguageTool
+;;
+
+;;; Code:
+
+(require 'lsp-mode)
+
+(defgroup lsp-languagetool nil
+  "Settings for the LTEX Language Server.
+
+https://github.com/languagetool-language-server/languagetool-languageserver"
+  :prefix "lsp-languagetool-"
+  :group 'lsp-mode
+  :link '(url-link :tag "Github" "https://github.com/emacs-languagetool/lsp-languagetool"))
+
+(provide 'lsp-languagetool)
+;;; lsp-languagetool.el ends here
