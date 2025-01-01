06d6a2a41465ea232b1b14544f6d728beb00ccdc
JenChieh <jcs090218@gmail.com>
2025-01-01 00:51:57 -0800

    Update copyright year

--- lsp-languagetool.el
+++ lsp-languagetool.el
@@ -1,6 +1,6 @@
 ;;; lsp-languagetool.el --- LSP Client for LanguageTool  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021-2024  Shen, Jen-Chieh
+;; Copyright (C) 2021-2025  Shen, Jen-Chieh
 ;; Created date 2021-04-05 23:30:46
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
